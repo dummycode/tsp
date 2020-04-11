71014d6df8b254f2492cd047e3deaf7ac1f98499
Henry Harris <henrythomasharris@gmail.com>
2020-04-11 12:22:44 -0400

    Read input and write output

--- tsp-3510.py
+++ tsp-3510.py
@@ -1,18 +1,65 @@
 import sys
+from collections import namedtuple
 
-def main(input_coords, output_tour, time):
+
+Point = namedtuple('Point', ['x', 'y'])
+
+
+def main(input_file, output_file, time):
     """
     Main driver for the program, kicks of search
     """
-    print("Program started ({}, {}, {})".format(input_coords, output_tour, time))
+    print("Program started ({}, {}, {})".format(input_file, output_file, time))
 
+    nodes = read_nodes(input_file)
+    print(nodes)
+    
+    tour = [0, 3, 20, 4, 5, 10]
+    write_results(69, tour)
+
+
+def read_nodes(input_file):
+    """
+    Reads in the nodes from the input file and creates
+    a list of points in memory
+    """
+    f = open(input_file, "r")
+
+    nodes = []
+    for line in f.readlines():
+        _, x_str, y_str = line.split(" ")
+        point = Point(float(x_str), float(y_str))
+        nodes.append(point)
+
+    return nodes
+
+
+def write_results(cost, tour):
+    """
+    Writes the results to the output file
+    """
+    f = open(output_file, "w")
+
+    f.write(str(cost))
+    f.write("\n")
+
+    for node in tour:
+        f.write("{} ".format(node))
+
+    f.write("\n")
+    f.close()
+
+    print("Results written to file")
+
+
+# If main, execute!
 if __name__ == "__main__":
     # Quit if invalid arguments
     if len(sys.argv) != 4:
         print("Invalid arguments!")
         quit(1)
     
-    _, input_coords, output_tour, time = sys.argv
+    _, input_file, output_file, time = sys.argv
 
-    main(input_coords, output_tour, time)
+    main(input_file, output_file, time)
 
