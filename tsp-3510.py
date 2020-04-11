cd471eb1bf4ee2ca3618f50ea8a3c5c0988cd823
Henry Harris <henrythomasharris@gmail.com>
2020-04-11 13:32:45 -0400

    Start of branch bound

--- tsp-3510.py
+++ tsp-3510.py
@@ -1,10 +1,15 @@
 import sys
 import math
+import numpy as np
 from collections import namedtuple
 from random import shuffle
 
 
 Point = namedtuple('Point', ['x', 'y'])
+
+nodes = []
+distances = []
+distances_np = None
 
 
 def main(input_file, output_file, time):
@@ -13,12 +18,54 @@ def main(input_file, output_file, time):
     """
     print("Program started ({}, {}, {})".format(input_file, output_file, time))
 
-    nodes = read_nodes(input_file)
-    print(nodes)
+    read_nodes(input_file)
+    gen_distances(nodes)
+    print(distances_np)
 
-    cost, tour = random_tour(nodes)
+    distances_reduced_rows_np = distances_np - np.min(distances_np, axis=1)[:, None]
+    distances_reduced_cols_np = distances_reduced_rows_np - np.min(distances_reduced_rows_np, axis=0)[None, :]
     
-    write_results(cost, tour)
+    print(distances_reduced_cols_np)
+
+    min_cost = float("inf")
+    min_tour = None
+
+    for i in range(100000):
+        cost, tour = random_tour(nodes)
+        if cost < min_cost:
+            min_cost = cost
+            min_tour = tour
+    
+    write_results(min_cost, min_tour)
+
+
+def gen_distances(nodes):
+    """
+    Generate the distances between all nodes
+    """
+    print("Generating distances between all nodes...")
+
+    # Setup node matrix
+    for i in range(len(nodes)):
+        distances.append([0] * len(nodes))
+
+    for i, n1 in enumerate(nodes):
+        for j, n2 in enumerate(nodes):
+            if i == j:
+                distances[i][j] = sys.maxsize
+            else:
+                distances[i][j] = euclidean_distance(n1, n2)
+    
+    # Convert to to numpy array
+    global distances_np
+    distances_np = np.array(distances, dtype=int)
+
+def get_distance(n1, n2):
+    """
+    Once distances have been generated, use this function to 
+    return the distance from the cache
+    """
+    return distances[n1][n2]
 
 
 def euclidean_distance(p1, p2):
@@ -39,7 +86,7 @@ def random_tour(points):
 
     cost = 0
     for i in range(len(tour) - 1):
-        cost += euclidean_distance(points[tour[i]], points[tour[i + 1]])
+        cost += get_distance(tour[i], tour[i + 1])
 
     return (cost, tour)
 
@@ -51,7 +98,6 @@ def read_nodes(input_file):
     """
     f = open(input_file, "r")
 
-    nodes = []
     for line in f.readlines():
         _, x_str, y_str = line.split(" ")
         point = Point(float(x_str), float(y_str))
